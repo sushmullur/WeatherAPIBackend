aa113dab04b00227692c11bed3ce60d9b0122b1a
sushmullur <mullur.sushruth@gmail.com>
2024-03-13 13:45:43 -0700

    Init template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+use lambda_runtime::{service_fn, LambdaEvent, Error};
+use serde_json::{Value, json};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = service_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+pub (crate) async fn my_handler(_event: LambdaEvent<Value>) -> Result<Value, Error> {
     println!("Hello, world!");
+    Ok(json!({ "message": "Hello, world!" }))
 }
