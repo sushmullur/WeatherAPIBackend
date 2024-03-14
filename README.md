b13a2cd441c88fc50ea5687dbe094f6a7fe43578
sushmullur <mullur.sushruth@gmail.com>
2024-03-13 23:01:23 -0700

    Successful API request parsing

---
+++ README.md
@@ -0,0 +1,5 @@
+# Deployment Instructions
+
+## 1. Install cargo-lambda
+## 2. Build the function with cargo lambda build --release 
+## 3. Deploy the function to AWS Lambda with cargo lambda deploy
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use lambda_runtime::{service_fn, LambdaEvent, Error};
 use serde_json::{Value, json};
+use std::collections::HashMap;
 
+
+// Comments are notes to self
+
+// Using the tokia::main attribute allows us to use async/await syntax on main method
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let func = service_fn(my_handler);
@@ -8,7 +13,14 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-pub (crate) async fn my_handler(_event: LambdaEvent<Value>) -> Result<Value, Error> {
-    println!("Hello, world!");
-    Ok(json!({ "message": "Hello, world!" }))
+// This is the handler function that will be called by the lambda runtime
+// pub (crate) restricts public access to within the crate
+pub (crate) async fn my_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
+    let payload = event.payload;
+
+    let data: HashMap<String, Value> = serde_json::from_value(payload)
+        .map_err(|e| Error::from(e.to_string()))?; 
+    let target_city = data.get("body").unwrap().as_str().unwrap();
+    
+    Ok(json!({ "city": target_city }))
 }
